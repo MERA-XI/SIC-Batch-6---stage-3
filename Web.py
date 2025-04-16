4353948674fa575cdceb55a08dbc4a6b03829e16
MERA-XI <merasmkn2@gmail.com>
2025-04-16 20:24:24 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -36,7 +36,7 @@ if Opsi == "Beranda":
     st.markdown("- Mendeteksi objek 🎯")
     col1, col2 = st.columns(2)
     with col1:
-        st.image("imgObjek.jpg")
+        st.image("gambar/imgObjek.jpg")
     with col2:
         st.empty()
     st.markdown("-----")
